d0d875697ff24d30ffae18be5392ad2f496c62ab
Ben Sutter <14796185+bms-1984@users.noreply.github.com>
2022-08-25 17:53:17 -0400

    organization, install script

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -159,3 +159,15 @@
   (global-set-key (kbd "C-c ,") 'previous-buffer)     
   (global-set-key (kbd "C-c .") 'next-buffer))        
 (load-theme 'solarized-dark t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/.dotfiles/TODO.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
